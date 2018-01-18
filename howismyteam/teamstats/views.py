575e4376722f1f814c0e0c2066e81cc49a2f8243
trolleksii <trolleksii@gmail.com>
2018-01-19 00:40:34 +0200

    added views for login, logout and dummy for results

--- howismyteam/teamstats/views.py
+++ howismyteam/teamstats/views.py
@@ -1,6 +1,22 @@
 from django.views.generic import TemplateView
+from django.contrib.auth.views import LoginView, LogoutView
 
 
 class IndexView(TemplateView):
 
     template_name = 'index.html'
+
+
+class UserLoginView(LoginView):
+
+    template_name = 'login.html'
+    extra_context = {'next': '/teamstats'}
+
+
+class UserLogoutView(LogoutView):
+
+    next_page = '/'
+
+
+class ResultsView(TemplateView):
+    template_name = 'results.html'
