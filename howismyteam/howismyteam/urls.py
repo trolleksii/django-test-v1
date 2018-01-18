a8bd1a5aeda6183a2ece339d3f6051c555e52040
trolleksii <trolleksii@gmail.com>
2018-01-19 00:39:40 +0200

    added urls for login, logout and results. Need to replace them inside app urls file

--- howismyteam/howismyteam/urls.py
+++ howismyteam/howismyteam/urls.py
@@ -14,11 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from teamstats import views
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
+    path('login/', views.UserLoginView.as_view(), name='login'),
+    path('logout/', views.UserLogoutView.as_view(), name='logout'),
+    path('teamstats/', include('teamstats.urls')),
     path('admin/', admin.site.urls),
 ]
