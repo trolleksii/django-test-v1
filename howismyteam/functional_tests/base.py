8bf9a10ac45eb3b24162f5d207e37792f044c63d
trolleksii <trolleksii@gmail.com>
2018-01-19 10:34:20 +0200

    registered models in the admin app, modified functional tests to check log in/out process

--- howismyteam/functional_tests/base.py
+++ howismyteam/functional_tests/base.py
@@ -2,11 +2,15 @@ from selenium import webdriver
 
 from unittest import TestCase
 
+from teamstats.models import User, Team, UserPollProfile
+
 
 class FunctionalTests(TestCase):
     """
     Base class with setUp, tearDown and other helper methods.
     """
+    test_users = []
+
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
--- howismyteam/functional_tests/test_index_page.py
+++ howismyteam/functional_tests/test_index_page.py
@@ -1,13 +1,32 @@
 import time
+
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.keys import Keys
+
 from .base import FunctionalTests
 
 
 class IndexPageTest(FunctionalTests):
     """
-    Simple test to check if django is working properly.
+    Bunch of test to check if index page and its elements are working properly.
     """
-    def test_django_landing_page(self):
-        # Project is still empty, but Django landing page should be available
+    def test_index_page_is_available(self):
         self.browser.get('http://localhost:8000')
         self.assertIn('How is my team?', self.browser.title)
-        time.sleep(10)
+
+    def test_user_can_log_in_and_out(self):
+        self.browser.get('http://localhost:8000')
+        # logging in
+        self.browser.find_element_by_id('id_log_in').click()
+        self.browser.find_element_by_id('id_username').send_keys('__testuser')
+        self.browser.find_element_by_id('id_password').send_keys('qwerty123')
+        self.browser.find_element_by_id('id_password').send_keys(Keys.ENTER)
+        # check if log in button disappeared
+        with self.assertRaises(NoSuchElementException):
+            self.browser.find_element_by_id('id_log_in')
+        # logging out
+        self.browser.find_element_by_id('id_log_out').click()
+        # check if log out button disappeared
+        with self.assertRaises(NoSuchElementException):
+            self.browser.find_element_by_id('id_log_out')
+        # time.sleep(20)
