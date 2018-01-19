4f7921ff806d7ae246b0683008582d7ca22e1b74
trolleksii <trolleksii@gmail.com>
2018-01-19 16:57:48 +0200

    added happiness calculation logic, moved poll date check out of the class

--- howismyteam/teamstats/models.py
+++ howismyteam/teamstats/models.py
@@ -1,8 +1,38 @@
 from datetime import date
 
-from django.db import models
+from django.core.exceptions import ObjectDoesNotExist
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.contrib.auth.models import User
+from django.db import models
+from django.db.models import Avg
+
+
+def get_happiness_stats(user):
+    """
+    Returns details on happines detais and an average in a tuple.
+    If user is in some team, stats are calculated for this team only. In other
+    case, stats are calculated for every user without a team.
+    """
+    try:
+        team_or_none = UserPollProfile.objects.get(user=user).team
+    except ObjectDoesNotExist:
+        # for user without profile get stats for all users without a team
+        team_or_none = None
+    detailed_happiness = [UserPollProfile.objects.filter(happiness=i, team=team_or_none).count() for i in range(1, 6)]
+    average_happiness = UserPollProfile.objects.filter(team=team_or_none).aggregate(Avg('happiness'))['happiness__avg']
+    return (detailed_happiness, average_happiness, )
+
+
+def user_was_polled_today(user):
+    """
+    Returns True if user was polled earlier this day.
+    """
+    try:
+        user_poll_profile = UserPollProfile.objects.get(user=user)
+    except ObjectDoesNotExist:
+        # for user without profile skip the poll
+        return True
+    return user_poll_profile.poll_date == date.today()
 
 
 class Team(models.Model):
@@ -44,12 +74,6 @@ class UserPollProfile(models.Model):
     # on the day when profile was created
     # So I decided to make it optional and set explicitly during the poll.
     poll_date = models.DateField(null=True, blank=True)
-
-    def was_polled_today(self):
-        """
-        Returns True if user was polled earlier this day.
-        """
-        return self.vote_date == date.today()
 
     def __str__(self):
         return self.user.username
