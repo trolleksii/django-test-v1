fc5d6ab1552b43369c01048c9b63ae3f3dbb7828
trolleksii <trolleksii@gmail.com>
2018-01-19 13:03:27 +0200

    urls are now uniform

--- howismyteam/howismyteam/urls.py
+++ howismyteam/howismyteam/urls.py
@@ -1,27 +1,10 @@
-"""howismyteam URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 
-from teamstats import views
+from teamstats.views import IndexView
 
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index'),
-    path('login/', views.UserLoginView.as_view(), name='login'),
-    path('logout/', views.UserLogoutView.as_view(), name='logout'),
+    path('', IndexView.as_view(), name='index'),
     path('teamstats/', include('teamstats.urls')),
     path('admin/', admin.site.urls),
 ]
--- howismyteam/teamstats/urls.py
+++ howismyteam/teamstats/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
 
+app_name = 'teamstats'
+
 urlpatterns = [
-    path('', views.ResultsView.as_view(), name='results'),
+    path('login', views.UserLoginView.as_view(), name='login_view'),
+    path('logout', views.UserLogoutView.as_view(), name='logout_view'),
+    path('poll', views.UserPollView.as_view(), name='userpoll_view'),
+    path('results', views.ResultsView.as_view(), name='results_view'),
 ]
