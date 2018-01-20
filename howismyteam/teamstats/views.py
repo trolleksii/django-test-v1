788138cb4aab474a52205b450f28398b0a447476
trolleksii <trolleksii@gmail.com>
2018-01-20 23:41:28 +0200

    UserPollView now change poll_date afret you press submit. Made redirector mixin that will route user to appropriate page.

--- howismyteam/teamstats/views.py
+++ howismyteam/teamstats/views.py
@@ -1,10 +1,13 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import LoginView, LogoutView
-from django.urls import reverse
+from django.shortcuts import redirect, reverse
 from django.views.generic import TemplateView, UpdateView
 
 from .forms import UserPollForm
-from .models import get_happiness_stats, UserPollProfile
+from .models import (
+    get_happiness_stats, is_eligible_for_poll, update_poll_date,
+    UserPollProfile
+)
 
 
 class IndexView(TemplateView):
@@ -26,7 +29,33 @@ class UserLogoutView(LogoutView):
     next_page = '/'
 
 
-class ResultsView(LoginRequiredMixin, TemplateView):
+class PollRedirectorMixin:
+    """
+    Redirects to poll page if the user is eligible.
+    Othewise redirets to the results page.
+    """
+
+    def dispatch(self, request, *args, **kwargs):
+        results_url = reverse('teamstats:results_view')
+        poll_url = reverse('teamstats:userpoll_view')
+        # Redirect is based of users ability to participate in a poll and
+        # the page he is trying to access. If user is eligible and is trying to
+        # access results page - he will be redirected to the poll page and
+        # vice versa.
+        redirection_table = {
+            (True, results_url): redirect(poll_url),
+            (False, poll_url): redirect(results_url)
+        }
+
+        eligibility = is_eligible_for_poll(self.request.user)
+        # redirect according to the table or invoke super().dispatch if
+        return redirection_table.get(
+            (eligibility, request.path),
+            super().dispatch(request, *args, **kwargs)
+        )
+
+
+class ResultsView(LoginRequiredMixin, PollRedirectorMixin, TemplateView):
 
     template_name = 'results.html'
 
@@ -41,7 +70,7 @@ class ResultsView(LoginRequiredMixin, TemplateView):
         return context
 
 
-class UserPollView(LoginRequiredMixin, UpdateView):
+class UserPollView(LoginRequiredMixin, PollRedirectorMixin, UpdateView):
 
     template_name = 'poll.html'
     form_class = UserPollForm
@@ -50,6 +79,8 @@ class UserPollView(LoginRequiredMixin, UpdateView):
         return reverse('teamstats:login_view')
 
     def get_success_url(self):
+        # poll was successfull, so we can save the date
+        update_poll_date(self.request.user)
         return reverse('teamstats:results_view')
 
     def get_object(self, querryser=None):
