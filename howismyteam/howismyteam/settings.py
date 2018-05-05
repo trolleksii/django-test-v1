bcfd37bbe63dae97f321c1f73f84a771d7909091
trolleksii <trolleksii@gmail.com>
2018-05-05 22:39:13 +0300

    settings for deploy

--- howismyteam/howismyteam/settings.py
+++ howismyteam/howismyteam/settings.py
@@ -19,13 +19,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '+bps^qsaz02mn0bn!&!8+p0x&hvzc$+hv68s)3#x^1kbn$tg(c'
+SECRET_KEY = os.environ.get('SECRET_KEY', '+bps^qsaz02mn0bn!&!8+p0x&hvzc$+hv68s)3#x^1kbn$tg(c')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'on') == 'on'
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost').split(',')
 
 # Application definition
 
@@ -121,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.environ.get('STATIC_ROOT', 'static/')
